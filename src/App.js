40bd276dfeb9d961a2235e91cb327f7b79163e3e
sergey_spynu <sspynu@tsconsulting.com>
2020-03-22 13:41:23 +0300

    functional component3

--- src/App.js
+++ src/App.js
@@ -9,6 +9,103 @@ export function App(props) {
 	const userName = props.name && <h1>Yo! { props.name }</h1>
 	const userAge = props.age && <h3>User age { props.age }</h3>
 
+	const users = [
+		{
+			id: 1,
+			name: "Иваныч",
+			age: 30,
+			gender: "M",
+			address: "Asdfj laskjd flsjkd f",
+			puppies: [],
+			description: "Какое-то описание",
+		}, {
+			id: 2,
+			name: "Семеныч",
+			age: 40,
+			gender: "M",
+			address: "ывра лыорвла ыорва f",
+			puppies: [],
+			description: "Какое-то описание 2",
+		}, {
+			id: 3,
+			name: "Татьяныч",
+			age: 50,
+			gender: "M",
+			address: "sdfoij wlkejf skndf",
+			puppies: [],
+			description: "Какое-то описание 3",
+		}, {
+			id: 7,
+			name: "Марьяна",
+			age: 25,
+			gender: "F",
+			address: "oiuwe oro4i3 453i4 5",
+			puppies: [],
+			description: "Какое-то описание 5",
+		}, {
+			id: 35,
+			name: "Ульяна",
+			age: 32,
+			gender: "А",
+			address: "456  43з56о дло дулцке",
+			puppies: [],
+			description: "Какое-то описание 5",
+		},
+
+	];
+	// свойство (prop) key - это аналог атрибута ID в HTML
+	// const renderUsers = (userInfo) => {
+	//
+	// 	return (<SimpleUser
+	// 		key={ userInfo.id }
+	// 		name={ userInfo.name }
+	// 		age={ userInfo.age }
+	// 		gender={ userInfo.gender }
+	// 		puppies={ userInfo.puppies }
+	// 		description={ userInfo.description }
+	// 	/>);
+	// };
+
+	// const renderUsers = ({
+	// 	                     id,
+	// 	                     name,
+	// 	                     gender,
+	// 	                     age,
+	// 	                     address,
+	// 	                     puppies,
+	// 	                     description,
+	//                      }) => {
+	//
+	// 	return (<SimpleUser
+	// 		key={ id }
+	// 		name={ name }
+	// 		age={ age }
+	// 		address={ address }
+	// 		gender={ gender }
+	// 		puppies={ puppies }
+	// 		description={ description }
+	// 	/>);
+	// };
+
+	// const renderUsers = ({
+	// 	                     id,
+	// 	                     name,
+	// 	                     gender,
+	// 	                     age,
+	// 	                     address,
+	// 	                     puppies,
+	// 	                     description,
+	//                      }) => (<SimpleUser
+	// 		key={ id }
+	// 		name={ name }
+	// 		age={ age }
+	// 		address={ address }
+	// 		gender={ gender }
+	// 		puppies={ puppies }
+	// 		description={ description }
+	// 	/>);
+
+
 	return (
     <div className="App">
 	    {
@@ -22,7 +119,27 @@ export function App(props) {
 	    }
 	    <User description="Some text" />
 
-	    <SimpleUser name="Иваныч" age={ 35 } />
+			<div style={ {display: "flex"} }>
+				{
+					users.map(({
+						            id,
+						            name,
+						            gender,
+						            age,
+						            address,
+						            puppies,
+						            description,
+					            }) => (<SimpleUser
+						key={ id }
+						name={ name }
+						age={ age }
+						address={ address }
+						gender={ gender }
+						puppies={ puppies }
+						description={ description }
+					/>))
+				}
+			</div>
 
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
